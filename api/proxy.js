f515843dcad9a559d3acb09a35563a0bad6429c6
josejosuetiago <58347924+josejosuetiago@users.noreply.github.com>
2024-12-19 15:39:21 -0300

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -3,16 +3,24 @@ import httpProxy from "http-proxy";
 const proxy = httpProxy.createProxyServer();
 
 export default (req, res) => {
-  const target = "wss://se.zclaro.com.br:443"; // Substitua pelo endereço da sua VPS.
+  const target = "wss://45.140.192.26:443"; // Substitua pelo endereço da sua VPS.
 
-  // Redirecionando a solicitação para a VPS
-  proxy.web(req, res, { target, changeOrigin: true }, (err) => {
-    console.error("Erro ao encaminhar a solicitação:", err);
-    res.status(502).send("Erro ao conectar ao servidor WebSocket.");
-  });
+  // Verificar o protocolo WebSocket
+  if (req.headers.upgrade && req.headers.upgrade.toLowerCase() === "websocket") {
+    proxy.ws(req, res, { target, changeOrigin: true }, (err) => {
+      console.error("Erro ao encaminhar WebSocket:", err);
+      res.status(502).send("Erro ao conectar ao servidor WebSocket.");
+    });
+  } else {
+    // Caso não seja WebSocket, tratar como requisição normal
+    proxy.web(req, res, { target, changeOrigin: true }, (err) => {
+      console.error("Erro ao encaminhar a solicitação:", err);
+      res.status(502).send("Erro ao conectar ao servidor.");
+    });
+  }
 
-  // Escutando erros de WebSocket
+  // Escutando erros do proxy
   proxy.on("error", (err) => {
-    console.error("Erro no proxy WebSocket:", err);
+    console.error("Erro no proxy:", err);
   });
 };
